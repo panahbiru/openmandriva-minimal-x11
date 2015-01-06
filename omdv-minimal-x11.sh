1f5e58af85e1ad28c0c39cc8e6105e1fbb11bd8e
Luthfi Emka <luthfi.emka@gmail.com>
2015-01-06 08:43:24 +0700

    remove log, add draklive-install-no-kdm

--- omdv-minimal-x11.sh
+++ omdv-minimal-x11.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#
+###########################################################
 #	OpenMandriva Lx Minimal X11 
+#	(c) 2015 Luthfi Emka <panahbiru[at]gmail[dot]com>
+#	License: GPL
 #
 ##########################################################
 
@@ -10,16 +14,16 @@ echo "Remove repository"
 urpmi.removemedia -a > $LOGFILE
 
 echo "Add personal Repo"
-urpmi.addmedia personal http://repo.unnes.ac.id/repo2/openmandriva > $LOGFILE
+urpmi.addmedia personal http://repo.unnes.ac.id/repo2/openmandriva
 
 echo "Install X11, xterm, omv-minsysreqs"
-urpmi task-x11 xterm omv-minsysreqs > $LOGFILE
-
-echo "Launch draklive install"
-/usr/sbin/draklive-install && startx > $LOGFILE
+urpmi --no-verify-rpm task-x11 xterm omv-minsysreqs draklive-install-no-kdm
 
 echo "Add distrib media"
-urpmi.addmedia --distrib http://kambing.ui.ac.id/unitylinux/openmandriva2014.0/repository/$ARCH > $LOGFILE
+urpmi.addmedia --distrib http://kambing.ui.ac.id/unitylinux/openmandriva2014.0/repository/$ARCH
+
+echo "Launch draklive install"
+startx | /usr/sbin/draklive-install
 
 echo "Reboot"
 reboot
